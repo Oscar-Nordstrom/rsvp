6ec997843007b7a7904ed0a78e0529880343f276
Oscar <oscarnordstrom@proton.me>
2026-09-08 21:51:06 +0200

    wip

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,7 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
-      <HelloWorld msg="Oscar & Afrodite" />
-    </div>
-  </header>
   <RouterView />
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,27 @@
 <script setup lang="ts"></script>
 
 <template>
-  <main>RSVP</main>
+  <div class="container">
+    <h1>Oscar & Afrodite</h1>
+    <div class="mainContent">
+      <h2>Welcome to our wedding website!</h2>
+      <h2>Plats</h2>
+      <h2>Tid 0000-00-00 00:00 - 00:00</h2>
+      <h2>Tema</h2>
+    </div>
+    <h1>❤️</h1>
+  </div>
 </template>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+}
+.mainContent {
+  text-align: center;
+}
+</style>
